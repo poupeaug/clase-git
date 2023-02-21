82eaa8886336531a85d68866ab3a3779392b619f
German Poupeau <poupeaug@gmail.com>
2023-02-21 18:07:38 -0300

    agrego clase Persona al script

--- script.py
+++ script.py
@@ -1,7 +1,9 @@
-from modulo import suma, restagi
+from modulo import suma, resta, Persona
 
 print("Hola Mundo")
 
 print (f"la suma de 1 + 2 = {suma(1,2)}")
 
 print (f"la resta de 2 - 1 = {resta(2,1)}")
+
+persona_1 = Persona()
